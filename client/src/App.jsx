39a93067cb21d9f99c24ff89babc50f3bcf230ab
EladRab1106 <eladrabinovitch1106@gmail.com>
2025-05-28 15:05:06 +0300

    components folder

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import axios from 'axios';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
-import CpuChart from './CpuChart';
+import CpuChart from './components/CpuChart';
 
 function App() {
   const now = new Date();
